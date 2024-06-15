f9dc8b6f3cfbc6201765b02be4134fb70a39708f
bedvoa <ujmn0418@gmail.com>
2024-06-15 14:16:31 +0900

    passwordStrengthCheck 테스트 코드 완성

--- passwordMeterTest/passwordStrengthCheck.js
+++ passwordMeterTest/passwordStrengthCheck.js
@@ -1,11 +1,81 @@
-const PasswordStrenth = {
+const PasswordStrength = {
   STRONG: "STRONG",
+  NORMAL: "NORMAL",
+  WEAK: "WEAK",
+  FAIL: "FAIL",
 };
 
-class PasswordStrenthCheck {
+class PasswordStrengthCheck {
   meter(password) {
-    return PasswordStrenth.STRONG;
+    // Test Case 2: 글자 길이가 8자 미만이고 모두 충족하는 경우
+    if (
+      password.length < 8 &&
+      /[a-z]/.test(password) &&
+      /[0-9]/.test(password)
+    ) {
+      return PasswordStrength.NORMAL;
+    }
+
+    // Test Case 3: 글자 길이가 8자 이상이고, 나머지 항목은 모두 충족하지 않는 경우
+    if (
+      !/[A-Z]/.test(password) &&
+      !/[0-9]/.test(password) &&
+      password.length >= 8
+    ) {
+      return PasswordStrength.WEAK;
+    }
+
+    // Test Case 4: 숫자는 없고, 나머지 항목은 모두 충족하는 경우
+    if (
+      !/[0-9]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /[A-Z]/.test(password) &&
+      password.length >= 8
+    ) {
+      return PasswordStrength.NORMAL;
+    }
+
+    // Test Case 5: 숫자는 있고, 나머지 항목은 모두 충족하지 않는 경우
+    if (
+      /[0-9]/.test(password) &&
+      password.length < 8 &&
+      !/[a-z]/.test(password)
+    ) {
+      return PasswordStrength.WEAK;
+    }
+
+    // Test Case 6: 대문자가 없고 나머지 항목은 충족하는 경우
+    if (
+      !/[A-Z]/.test(password) &&
+      /[0-9]/.test(password) &&
+      /[a-z]/.test(password) &&
+      password.length >= 8
+    ) {
+      return PasswordStrength.NORMAL;
+    }
+
+    // Test Case 7: 대문자가 있고 나머지 항목은 충족하지 않는 경우
+    if (
+      /[A-Z]/.test(password) &&
+      password.length < 8 &&
+      !/[0-9]/.test(password)
+    ) {
+      return PasswordStrength.WEAK;
+    }
+
+    // Test Case 8: 모든 조건을 충족하지 않는 경우
+    if (
+      !/[A-Z]/.test(password) &&
+      !/[0-9]/.test(password) &&
+      !/[a-z]/.test(password) &&
+      password.length < 8
+    ) {
+      return PasswordStrength.FAIL;
+    }
+
+    // Test Case 1: 모든 조건을 충족하는 경우
+    return PasswordStrength.STRONG;
   }
 }
 
-module.exports = { PasswordStrenthCheck };
+module.exports = { PasswordStrengthCheck };
--- passwordMeterTest/test/passwordStrenthTest.test.js
+++ passwordMeterTest/test/passwordStrenthTest.test.js
@@ -1,10 +1,59 @@
-const { PasswordStrenthCheck } = require("../passwordStrengthCheck");
+const { PasswordStrengthCheck } = require("../passwordStrengthCheck");
 
 describe("암호 검사기 - Test Code", () => {
-  it("Test Case: 모든 조건을 충족하는 경우", () => {
-    const meter = new PasswordStrenthCheck();
+  it("Test Case 1: 모든 조건을 충족하는 경우", () => {
+    const meter = new PasswordStrengthCheck();
     const result = meter.meter("aA1!aaaa"); // Expected: STRONG
 
     expect(result).toBe("STRONG");
+  });
+
+  it("Test Case 2: 길이가 8자 미만이고 모두 충족하는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("aA1!aa"); // Expected: NORMAL
+
+    expect(result).toBe("NORMAL");
+  });
+
+  it("Test Case 3: 길이가 8자 글자 이상이고, 나머지 항목은 모두 충족하지 않는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("aaaaaaaa"); // Expected: WEAK
+
+    expect(result).toBe("WEAK");
+  });
+
+  it("Test Case 4: 숫자는 없고, 나머지 항목은 모두 충족하는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("aA!aaaafdd"); // Expected: NORMAL
+
+    expect(result).toBe("NORMAL");
+  });
+
+  it("Test Case 5: 숫자는 있고, 나머지 항목은 모두 충족하지 않는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("12345"); // Expected: WEAK
+
+    expect(result).toBe("WEAK");
+  });
+
+  it("Test Case 6: 대문자가 없고 나머지 항목은 충족하는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("a1!aaaa"); // Expected: NORMAL
+
+    expect(result).toBe("NORMAL");
+  });
+
+  it("Test Case 7: 대문자가 있고 나머지 항목은 충족하지 않는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter("AZasd"); // Expected: WEAK
+
+    expect(result).toBe("WEAK");
+  });
+
+  it("Test Case 8: 모든 조건을 충족하지 않는 경우", () => {
+    const meter = new PasswordStrengthCheck();
+    const result = meter.meter(""); // Expected: FAIL
+
+    expect(result).toBe("FAIL");
   });
 });
