25042eef128616fbdbab28a9edfea1e3ffef658b
bedvoa <ujmn0418@gmail.com>
2024-07-06 10:42:08 +0900

    money problem: RGR

--- tdd-oreilly/money-problem/money.test.ts
+++ tdd-oreilly/money-problem/money.test.ts
@@ -1,5 +1,21 @@
-describe("money problem", () => {
-  it("should return 1", () => {
-    expect(1).toBe(1);
+import assert from "assert";
+
+class Dollar {
+  amount: number;
+
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  times(multiplier: number) {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+describe("first", () => {
+  it("5 USD * 2 = 10 USD", () => {
+    let fiver = new Dollar(5);
+    let tenner = fiver.times(2);
+    assert.strictEqual(tenner.amount, 10);
   });
 });
