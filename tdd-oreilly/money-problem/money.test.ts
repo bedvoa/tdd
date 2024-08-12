7f9cd9e8446589b31f306d4f776e25c12315ee69
bedvoa <ujmn0418@gmail.com>
2024-08-12 22:07:40 +0900

    feat: division and multiplication features done

--- tdd-oreilly/money-problem/money.test.ts
+++ tdd-oreilly/money-problem/money.test.ts
@@ -22,8 +22,22 @@ describe("Money Problem", () => {
     });
   });
 
-  // describe("환전 계산", () => {
-  //   it("5 USD + 10 EUR = 17 USD", () => {});
-  //   it("1 USD + 1100KRW = 2200KRW", () => {});
-  // });
+  describe("다양한 통화로 돈 계산", () => {
+    it("EUR; '10 EUR'를 나타내는 개체에 2를 곱하면 '20 EUR'가 되는지 검증", () => {
+      const tenEuros = new Money(10, "EUR");
+      const twentyEuros = tenEuros.times(2);
+      expect(twentyEuros.amount).toBe(20);
+      expect(twentyEuros.currency).toBe("EUR");
+      expect(twentyEuros).toStrictEqual(new Money(20, "EUR"));
+    });
+
+    it("KRW; '4002 KRW'를 나타내는 개체를 4로 나누면 '1000.5 KRW'가 되는지 검증", () => {
+      const originalMoney = new Money(4002, "KRW");
+      const actualMoneyAfterDivision = originalMoney.divide(4);
+      const expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+      expect(actualMoneyAfterDivision).toStrictEqual(
+        expectedMoneyAfterDivision
+      );
+    });
+  });
 });
